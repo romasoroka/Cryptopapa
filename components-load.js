dd3b01a80bf8b9509f5da25eacf5a0216d365699
Roman Soroka <roman.soroka@gmail.com>
2025-09-16 10:40:08 +0300

    First commit to Development branch

---
+++ components-load.js
@@ -0,0 +1,15 @@
+// JavaScript to fetch and insert header.html
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("afterbegin", data);
+    })
+    .catch((error) => console.error("Error loading header:", error));
+  fetch("/components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("afterbegin", data);
+    })
+    .catch((error) => console.error("Error loading header:", error));
+});
